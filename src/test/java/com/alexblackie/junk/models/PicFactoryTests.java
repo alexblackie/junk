efea27d496b8fca08285928161529fc0bad5e012
Alex Blackie <alex@blackie.me>
2022-04-04 22:01:41 -0700

    Fix name of PicFactoryTests
    Failed to copy-paste once again.

--- src/test/java/com/alexblackie/junk/models/PicFactoryTests.java
+++ src/test/java/com/alexblackie/junk/models/PicFactoryTests.java
@@ -14,7 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class JunkApplicationTests {
+class PicFactoryTests {
 
 	@Test
 	void testPicFactory_buildPic_withPrefix_noImage() {
